0f3c82c7924b5e2564c205bfd9f90b01d3f22ebe
Joshua Shireman <kealist@gmail.com>
2013-07-20 10:29:41 -0500

    playsound.r first submission
    Started an extension to use winmm.dll 's function "playsound" but not
    too much progress yet

---
+++ dll/playsound.r
@@ -0,0 +1,7 @@
+REBOL []
+
+PlaySound: make routine! [
+    "The PlaySound function plays a sound specified by the given file name, resource, or system event."
+    argument [type] "Arg description string"
+    return: [type]
+] library func-id
